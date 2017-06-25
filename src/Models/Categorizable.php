6b2bd9616f8253e7df564899d3a703acb17572a8
Top Mora <manager@topmora.com>
2017-06-25 19:13:41 +0200

    Kipa : selection categories

--- src/Models/Categorizable.php
+++ src/Models/Categorizable.php
@@ -14,11 +14,42 @@ class Categorizable extends Model
 	
 	protected $with = ["category"];
 	
+	private static $sels = [];
+	
 	function categorizable() {
 		return $this->morphTo();
 	}
 	
 	public function category() {
 		return $this->belongsTo("Ry\Categories\Models\Categorie", "categorie_id");
+	}
+	
+	private static function selected($ar) {
+		foreach ($ar as $a) {
+			if(isset($a["selected"]) && $a["selected"]==true) {
+				self::$sels[] = $a;
+			}
+			self::selected($a["children"]);
+		}
+	}
+	
+	public static function forTree($tree, $cast, $notIn = [], $in = []) {		
+		self::$sels = [];
+		self::selected($tree);
+		$arcat = [];
+		foreach(self::$sels as $categorie) {
+			$arcat[] = $categorie["id"];
+		}
+		$query = Categorizable::whereIn("categorie_id", $arcat)->where("categorizable_type", "=", $cast);
+		if(count($notIn)>0)
+			$query->whereNotIn("categorizable_id", $notIn);
+		if(count($in)>0)
+			$query->whereIn("categorizable_id", $in);
+		$cats = $query->get();
+		$ar = [];
+		foreach($cats as $cat)
+			$ar[] = $cat->categorizable;
+		
+		return $ar;
 	}
 }
